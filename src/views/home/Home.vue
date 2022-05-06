8e156e93990a6000ac4dfabd5ecdf7d582ab7d3d
jiran201 <869593223@qq.com>
2022-05-06 15:01:12 +0800

    first commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="wrapper">
+    <StaticPart />
+    <Nearby />
+    <Docker :currentIndex="0"/>
+  </div>
+</template>
+
+<script>
+import StaticPart from './StaticPart'
+import Nearby from './Nearby'
+import Docker from '../../components/Docker'
+export default {
+  name: 'Home',
+  components: { StaticPart, Nearby, Docker }
+}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  overflow-y: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  padding: 0 .18rem .1rem .18rem;
+}
+</style>
